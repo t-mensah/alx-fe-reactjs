2b1481dc01c916c62f1fec39d646b89d3aec9fbe
t-mensah <theophilusmensah2023@gmail.com>
2025-10-30 22:54:49 +0000

    update App.jsx with User Profile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,25 +1,26 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
       <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
-      {/* Vite default section */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +29,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ const UserProfile = (props) => {
     <div>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
