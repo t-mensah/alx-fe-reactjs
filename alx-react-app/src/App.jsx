ed771e420f4b9cab46f28836c3c7905ad6cf26ed
t-mensah <theophilusmensah2023@gmail.com>
2025-10-30 01:55:19 +0000

    update Footer, Header, and ManiContent

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,22 +1,23 @@
-
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <WelcomeMessage />
-    <Header />
-    <MainContent />
-    <Footer/>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+
+      {/* Vite default section */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -25,6 +26,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -38,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
