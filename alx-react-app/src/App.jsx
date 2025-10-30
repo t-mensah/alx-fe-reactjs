38dbee73703b2bfd6300b6c007561746040e9104
t-mensah <theophilusmensah2023@gmail.com>
2025-10-30 01:15:57 +0000

    added Header, mainContent, and footer to the src

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,8 @@
 
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,6 +14,9 @@ function App() {
   return (
     <>
     <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
