337dabf7a81f18d9092f9a49b8030d377152bca2
t-mensah <theophilusmensah2023@gmail.com>
2025-11-09 00:06:00 +0000

    alx-react-app-new: add styled Header, MainContent, Footer, UserProfile with inline CSS

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f5f5f5', textAlign: 'center', color:'black' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
