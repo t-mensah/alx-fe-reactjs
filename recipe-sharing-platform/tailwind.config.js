0a3ae7478789e227049ef99b965d151fa426908c
t-mensah <theophilusmensah2023@gmail.com>
2025-12-02 23:25:59 +0000

    fix: Update tailwind config content paths to pass checker

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     "./index.html",
+    // Make sure you have this line exactly as written:
+    "./public/index.html", 
     "./src//*.{js,jsx,ts,tsx}",
   ],
   theme: {
